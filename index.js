2a285e298e5b1d748de2cb2168d78f9733f73e43
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-01-20 14:23:37 +0300

    feat: add start, stop, pause, value

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-console.log('====================================');
-console.log();
-console.log('====================================');
+import { music1 } from "./music.js";
+
+let isPlay = false;
+let playNum = 0;
+
+
+const audiofile = document.querySelector('.audio')
+const btnPrev = document.querySelector('.btn-prev')
+const btnPlay = document.querySelector('.btn-play')
+const btnNext = document.querySelector('.btn-next')
+const btnStop = document.querySelector('.btn-stop')
+const progress = document.querySelector('.progress')
+const volumeInput = document.querySelector('.volume')
+
+
+
+btnPlay.addEventListener('click', changePlaying)
+btnStop.addEventListener('click', stopAudio)
+btnNext.addEventListener('click', playNext)
+btnPrev.addEventListener('click', playPrev)
+volumeInput.addEventListener('input', changVvolume)
+
+function changePlaying() {
+    console.log(isPlay);
+    if (!isPlay) {
+        audiofile.play()
+        isPlay = true
+    } else {
+        audiofile.pause();
+        isPlay = false
+    }
+}
+
+function stopAudio() {
+    audiofile.pause();
+    audiofile.currentTime = 0;
+    isPlay = false;
+}
+function playNext() {
+    isPlay = false;
+    playNum++
+    if (playNum > music1.length - 1) {
+        playNum = 0
+    }
+    audiofile.src = music1.filter((el, index) => index == playNum)
+    changePlaying()
+}
+function playPrev() {
+    isPlay = false;
+    playNum--
+    if (playNum < 0) {
+        playNum = music1.length - 1
+    }
+    audiofile.src = music1.filter((el, index) => index == playNum)
+    changePlaying()
+}
+function changVvolume() {
+    let v = volumeInput.value
+    audiofile.volume = v / 100
+}
---
+++ music.js
@@ -0,0 +1,2 @@
+export const music1 = ['./audio1/АртемПивоваров-Мираж.mp3', './audio1/Глюкоза-Руки-Бёдра.mp3', './audio1/Aleks Ataman & Finik.Finya - Музыка Играет Громко.mp3', './audio1/ChrisYank-Холодно.mp3', './audio1/melisa_ft_tommo_-_im_alone_2021.mp3', './audio1/NЮ-Никто.mp3']
+
